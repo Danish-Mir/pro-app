59f6387a8e7ba48bd8ba44ba05cd4d0d8f6d5e4e
Danish-Mir <danishmir1996@gmail.com>
2023-11-09 06:33:41 -0800

    Project initiated

--- src/App.js
+++ src/App.js
@@ -9,12 +9,7 @@ function App() {
   return (
     <div>
       <Header />
-      <main className='py-3'>
-        <Container>
-        <h1>Welcome</h1>
-        </Container>
-        
-      </main>
+
       <Footer/>
     </div>
      );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div>
       <footer>
-
+     This is an ecommerce project  &copy; Danish Mir
       </footer>
     </div>
   )
